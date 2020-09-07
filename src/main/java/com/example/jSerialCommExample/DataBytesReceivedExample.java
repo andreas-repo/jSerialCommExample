b8bf7d40dfaa85a907c0c826eed0a6d9661454fe
andreas-repo <andreas.mayer@gepardec.com>
2020-09-07 13:15:21 +0200

    -- DataBytesReceived Example

---
+++ src/main/java/com/example/jSerialCommExample/DataBytesReceivedExample.java
@@ -0,0 +1,25 @@
+package com.example.jSerialCommExample;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+public class DataBytesReceivedExample {
+    public static void main(String[] args) {
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+        comPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
+            @Override
+            public void serialEvent(SerialPortEvent event)
+            {
+                byte[] newData = event.getReceivedData();
+                System.out.println("Received data of size: " + newData.length);
+                for (int i = 0; i < newData.length; ++i)
+                    System.out.print((char)newData[i]);
+                System.out.println("\n");
+            }
+        });
+    }
+}
